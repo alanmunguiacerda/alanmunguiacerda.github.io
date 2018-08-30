41cab791157b60bc5d63e03a8f71e931bbaf87ff
Alan Munguía <alan.munguia@alumnos.udg.mx>
2018-08-29 23:27:16 -0500

    Add act_2 win

--- sor/act_2.js
+++ sor/act_2.js
@@ -39,8 +39,23 @@ const data = {
       data: [
         {
           image: 'media/act_2/win1.png',
-          title: '',
-          description: ''
+          title: 'Instalar PSUTIL',
+          description: 'Como primer paso, es necesario instalar la librería "psutil".'
+        },
+        {
+          image: 'media/act_2/win2.png',
+          title: 'Crear un nuevo archivo "main.py"',
+          description: 'Aquí escribiremos el código para leer los procesos activos. El código es bastante simple, importamos la librería, leemos los "pids" de los procesos e imprimimos el nombre de cada uno.'
+        },
+        {
+          image: 'media/act_2/win3.png',
+          title: 'Ejecución',
+          description: 'Cerramos el archivo y lo ejecutamos.'
+        },
+        {
+          image: 'media/act_2/win4.png',
+          title: 'Resultado',
+          description: 'Los nombres de los procesos deberían de aparecer.'
         },
         {
           icon: 'media/download.png',
